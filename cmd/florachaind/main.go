758e8dd194d54e416c5ee0954d7802492c6ed072
Developer Experience team at Ignite <hello@ignite.com>
2025-09-20 17:21:13 +0000

    Initialized with Ignite CLI

---
+++ cmd/florachaind/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	clienthelpers "cosmossdk.io/client/v2/helpers"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/metaflora/florachain/app"
+	"github.com/metaflora/florachain/cmd/florachaind/cmd"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, clienthelpers.EnvPrefix, app.DefaultNodeHome); err != nil {
+		fmt.Fprintln(rootCmd.OutOrStderr(), err)
+		os.Exit(1)
+	}
+}
